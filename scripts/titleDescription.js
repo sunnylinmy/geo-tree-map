a938f19833da53e89e5799e109c0ea557aef3300
yiannisnet <yiannisnet@gmail.com>
2010-10-22 23:15:56 +0000

    added dc and title

---
+++ scripts/titleDescription.js
@@ -0,0 +1,27 @@
+
+function setTitleDescription(obj, desc) {
+	  if (desc == "total") {
+		  desc = "total number of people employed";
+	  } else if (desc == "annualMeanWage") {
+		  desc = "annual mean wage";	
+	  } else if (desc == "ann10Wage") {
+		  desc = "annual 10th percentile wage";	
+	  } else if (desc == "ann25Wage") {
+		  desc = "annual 25th percentile wage";	
+	  } else if (desc == "ann50Wage") {
+		  desc = "annual 50th percentile wage";	
+	  } else if (desc == "ann75Wage") {
+		  desc = "annual 75th percentile wage";	
+	  } else if (desc == "ann90Wage") {
+		  desc = "annual 90th percentile wage";	
+	  }
+	  obj = document.getElementById(obj);
+	  var description = document.createTextNode("Map Showing the " + desc + " per state");
+	  obj.appendChild(description);
+}
+
+function clearContainer(container) {
+	  while (container.hasChildNodes()) {
+	    container.removeChild(container.lastChild);
+	  }
+}
