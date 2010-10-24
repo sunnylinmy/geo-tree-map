a8edd6fc6dcda991fbf0fdb075a248fe87f892a5
yiannisnet <yiannisnet@gmail.com>
2010-10-24 03:31:11 +0000

    styled popup div added title

--- scripts/popupDiv.js
+++ scripts/popupDiv.js
@@ -1,30 +1,41 @@
 x = 0;
 y = 0;
-function setVisible(obj, stateCode, occupation, show) {
+function setVisible(obj, state, occupation, show) {
 	setTimeout("placeIt('popupData')",0); 
 	obj = document.getElementById(obj);
 	if (show == true) {
 		obj.style.visibility = 'visible'
 		if (level(occupation) != 1) {
-			var dataLbl = "Total % Employed: " + getValue(stateCode, occupation, "total") + "%";		
+			var dataLbl = "Total % Employed: " + getValue(state.code, occupation, "total") + "%";		
 		} else {
-			var dataLbl = "Total Employed: " + getValue(stateCode, occupation, "total");
+			var dataLbl = "Total Employed: " + getValue(state.code, occupation, "total");
 		}
+		var ocupationInfo;
+		if (occupation.subcategory) {
+			occupationInfo = occupation.subcategory;
+		} else if (occupation.category) {
+			occupationInfo = occupation.category;
+		}
+		var titleDiv = document.createElement('div');
+		titleDiv.setAttribute("id", "titlePopupDiv");
+		var popupTitleLabel = document.createTextNode(occupationInfo + " in " + state.name);
+		titleDiv.appendChild(popupTitleLabel);
 		var totalLabel = document.createTextNode(dataLbl);
-		dataLbl = "Annual Mean Wage: " + getValue(stateCode, occupation, "annualMeanWage");
+		dataLbl = "Annual Mean Wage: " + getValue(state.code, occupation, "annualMeanWage");
 		var annualMeanWageLabel = document.createTextNode(dataLbl);
-		dataLbl = "Annual 10% Wage: " + getValue(stateCode, occupation, "ann10Wage");
+		dataLbl = "Annual 10% Wage: " + getValue(state.code, occupation, "ann10Wage");
 		var ann10WageLabel = document.createTextNode(dataLbl);
-		dataLbl = "Annual 25% Wage: " + getValue(stateCode, occupation, "ann25Wage");
+		dataLbl = "Annual 25% Wage: " + getValue(state.code, occupation, "ann25Wage");
 		var ann25WageLabel = document.createTextNode(dataLbl);
-		dataLbl = "Annual Median Wage: " + getValue(stateCode, occupation, "ann50Wage");
+		dataLbl = "Annual Median Wage: " + getValue(state.code, occupation, "ann50Wage");
 		var ann50WageLabel = document.createTextNode(dataLbl);
-		dataLbl = "Annual 75% Wage: " + getValue(stateCode, occupation, "ann75Wage");
+		dataLbl = "Annual 75% Wage: " + getValue(state.code, occupation, "ann75Wage");
 		var ann75WageLabel = document.createTextNode(dataLbl);
-		dataLbl = "Annual 90% Wage: " + getValue(stateCode, occupation, "ann90Wage");
+		dataLbl = "Annual 90% Wage: " + getValue(state.code, occupation, "ann90Wage");
 		var ann90WageLabel = document.createTextNode(dataLbl);
 		clearContainer(obj);
-		obj.appendChild(document.createElement('br'));
+		
+		obj.appendChild(titleDiv);
 		obj.appendChild(totalLabel);
 		obj.appendChild(document.createElement('br'));
 		obj.appendChild(annualMeanWageLabel);
