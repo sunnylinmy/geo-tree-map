62b9bd8c5d01e0060b49d3f9e1931b61a2bb39af
andrew.d.hershberger <andrew.d.hershberger@97e81679-2e40-039e-0f97-0751f2aa5004>
2010-10-28 05:07:43 +0000

    updated layout and titles;

--- scripts/titleDescription.js
+++ scripts/titleDescription.js
@@ -1,38 +1,49 @@
 function setTitleDescription(obj, measure, occupation) {
-	  if (measure == "total") {
-		  if (level != 1) {
-			  measure = "% of people employed";
-		  } else {
-			  measure = "total number of people employed";			  
-		  }
-	  } else if (measure == "annualMeanWage") {
-		  measure = "annual mean wage";	
-	  } else if (measure == "ann10Wage") {
-		  measure = "annual 10th percentile wage";	
-	  } else if (measure == "ann25Wage") {
-		  measure = "annual 25th percentile wage";	
-	  } else if (measure == "ann50Wage") {
-		  measure = "annual 50th percentile wage";	
-	  } else if (measure == "ann75Wage") {
-		  measure = "annual 75th percentile wage";	
-	  } else if (measure == "ann90Wage") {
-		  measure = "annual 90th percentile wage";	
-	  }
-	  obj = document.getElementById(obj);
-	  var text = "Map Showing the " + measure + " per state in 2009";
-	  
-	  if (occupation.subcategory) {
-		  text += occupation.subcategory;
-	  } else if (occupation.category) {
-		  text += " for " + occupation.category;
-	  }
-	  var description = document.createTextNode(text);
-	  clearContainer(obj);
-	  obj.appendChild(description);
+	
+	var line1 = ""; // measure name
+	var line2 = ""; // category/subcategory
+	var line3 = ""; // occupation title
+	
+	if (measure == "total") {
+		if (level != 1) {
+			line1 = "Percent of employees";
+		} else {
+			line1 = "Total number of employees";				
+		}
+	} else if (measure == "annualMeanWage") {
+		line1 = "Annual mean wage";	
+	} else if (measure == "ann10Wage") {
+		line1 = "Annual 10th percentile wage";	
+	} else if (measure == "ann25Wage") {
+		line1 = "Annual 25th percentile wage";	
+	} else if (measure == "ann50Wage") {
+		line1 = "Annual 50th percentile wage";	
+	} else if (measure == "ann75Wage") {
+		line1 = "Annual 75th percentile wage";	
+	} else if (measure == "ann90Wage") {
+		line1 = "Annual 90th percentile wage";	
+	}
+	
+	titleDiv = document.getElementById(obj);
+	
+	if (occupation.subcategory) {
+		line2 = "in category";
+		line3 = occupation.subcategory;
+	} else if (occupation.category) {
+		line2 = "in subcategory";
+		line3 = occupation.category;
+	}
+	
+	var lineNodes = [document.createTextNode(line1), document.createTextNode(line2), document.createTextNode(line3)];
+	var childNodes = titleDiv.childNodes;
+	for (var i=0; i<3; i++) {
+		clearContainer(childNodes[i]);
+		childNodes[i].appendChild(lineNodes[i]);
+	}
 }
 
 function clearContainer(container) {
-	  while (container.hasChildNodes()) {
-	    container.removeChild(container.lastChild);
-	  }
+	while (container.hasChildNodes()) {
+		container.removeChild(container.lastChild);
+	}
 }
